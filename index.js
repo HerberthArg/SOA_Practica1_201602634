202873482a707a73b2fbe924a32cf817a4cb0bc0
Herberth Argueta <herbertharg@MacBook-Air-de-Herberth.local>
2020-12-16 23:13:07 -0600

    Practica 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/login", (req, res) => {
-  if (1 == 1) res.status(200).json({ mensaje: "Pruebas correctass" });
+  if (1 == 2) res.status(200).json({ mensaje: "Pruebas correctass" });
   else res.status(500).json({ mensaje: "Pruebas incorrectas" });
 });
 
