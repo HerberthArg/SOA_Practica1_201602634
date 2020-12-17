cf0ef897dbbddefc2ef7161f9540a2b262df1eca
Herberth Argueta <herbertharg@MacBook-Air-de-Herberth.local>
2020-12-16 21:31:37 -0600

    Practica 2

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/login', (req, res) => {
-    if(1==2)
-        res.status(200).json({mensaje: "Pruebas correctass"})
-    else
-        res.status(500).json({mensaje: "Pruebas incorrectas"})
-
-
-    
+app.get("/login", (req, res) => {
+  if (1 == 1) res.status(200).json({ mensaje: "Pruebas correctass" });
+  else res.status(500).json({ mensaje: "Pruebas incorrectas" });
 });
 
 app.listen(port, () => {
-    console.log(`Server en puerto ${port}`);
+  console.log(`Server en puerto ${port}`);
 });
